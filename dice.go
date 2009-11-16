be01a053e96af65e5361fb70fe9810307b4282ab
dowaito <dowaitodesu@gmail.com>
2009-11-15 19:57:12 -0700

    + Moved string parsing to roll. + Added format flag to roll.

--- dice.go
+++ dice.go
@@ -3,24 +3,9 @@ package dice
 import (
 	"time";
 	"rand";
-	"strings";
-	"strconv";
 )
 
-func Roll(dformat string) int {
-	var dtype, times int;
-	var parsedDice []string = strings.Split(dformat, "d", 0);
-	if parsedDice[1] == "F" || parsedDice[0] == "f" {
-		dtype = 0;
-	} else {
-		dtype,_ = strconv.Atoi(parsedDice[1]);
-	}
-	times,_ = strconv.Atoi(parsedDice[0]);
-
-	return Nroll(dtype, times);
-}
-
-func Nroll(dtype, times int) int {
+func Roll(dtype, times int) int {
 	total := 0;
 	for i := 0; i < times; i++ {
 		rand.Seed(time.Nanoseconds());
--- roll.go
+++ roll.go
@@ -5,10 +5,40 @@ import (
 	"fmt";
 	"flag";
 	"strings";
+	"strconv";
 )
 
+var format = flag.Bool("f", false, "Print roll with list of individual rolls")
 
 func main() {
 	flag.Parse();
-	fmt.Print(dice.Roll(strings.ToLower(flag.Arg(0))), "\n");
+	var dtype, times int;
+	dtype, times = parseRoll(strings.ToLower(flag.Arg(0)));
+
+	if !*format {
+		fmt.Print(dice.Roll(dtype, times), "\n");
+	} else {
+		var roll, total int;
+		rolls := make([]string,times);
+		for i := 0; i < times; i++ {
+			roll = dice.Roll(dtype, 1);
+			rolls[i] = strconv.Itoa(roll);
+			total += roll;
+		}
+		fmt.Print("[",strings.Join(rolls,", "),"] ", total, "\n");
+	}
 }
+
+func parseRoll(dformat string) (int,int) {
+	var dtype, times int;
+	var parsedDice []string = strings.Split(dformat, "d", 0);
+	if parsedDice[1] == "F" || parsedDice[0] == "f" {
+		dtype = 0;
+	} else {
+		dtype,_ = strconv.Atoi(parsedDice[1]);
+	}
+	times,_ = strconv.Atoi(parsedDice[0]);
+
+	return dtype, times;
+}
+
